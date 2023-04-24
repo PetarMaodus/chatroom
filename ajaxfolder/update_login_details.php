958d5b0eb7bbfb592f80500e78680f12f0853c3b
PetarMaodus <maodus.petar@gmail.com>
2023-04-24 10:42:50 +0200

    Summary
    cleared a little the code

--- ajaxfolder/update_login_details.php
+++ ajaxfolder/update_login_details.php
@@ -1,27 +1,25 @@
 <?php
 session_start();
-require_once "../config.php"; // Include your database configuration file
+require_once "../config.php";
 
-// Check if id parameter is passed from AJAX request
 if(isset($_POST['id'])){
     $id = $_POST['id'];
     
-    // Prepare and execute the UPDATE statement
     $sql = "UPDATE login_details SET last_activity=? WHERE id=?";
     if ($stmt = $mysqli->prepare($sql)) {
         $new_last_activity_param = date("Y-m-d H:i:s");
         $stmt->bind_param("si", $new_last_activity_param, $id);
         if ($stmt->execute()) {
             $_SESSION["lastseen"] = $new_last_activity_param;
-            echo "success"; // Return success response to AJAX request
+            echo "success"; 
         } else {
-            echo "error"; // Return error response to AJAX request
+            echo "error"; 
         }
         $stmt->close();
     } else {
-        echo "error"; // Return error response to AJAX request
+        echo "error"; 
     }
 } else {
-    echo "error"; // Return error response to AJAX request
+    echo "error"; 
 }
 ?>
--- copy.php
+++ copy.php
@@ -1,24 +1,14 @@
 <?php
-// Initialize the session
 session_start();
- 
-// Check if the user is logged in, otherwise redirect to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
 }
- 
-// Include config file
 require_once "config.php";
- 
-// Define variables and initialize with empty values
 $new_password = $confirm_password = "";
 $new_password_err = $confirm_password_err = "";
- 
-// Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
  
-    // Validate new password
     if(empty(trim($_POST["new_password"]))){
         $new_password_err = "Please enter the new password.";     
     } elseif(strlen(trim($_POST["new_password"])) < 6){
@@ -27,7 +17,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $new_password = trim($_POST["new_password"]);
     }
     
-    // Validate confirm password
     if(empty(trim($_POST["confirm_password"]))){
         $confirm_password_err = "Please confirm the password.";
     } else{
@@ -37,22 +26,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         }
     }
         
-    // Check input errors before updating the database
     if(empty($new_password_err) && empty($confirm_password_err)){
-        // Prepare an update statement
         $sql = "UPDATE users SET User_password = ? WHERE id = ?";
         
         if($stmt = $mysqli->prepare($sql)){
-            // Bind variables to the prepared statement as parameters
             $stmt->bind_param("si", $param_password, $param_id);
             
-            // Set parameters
             $param_password = password_hash($new_password, PASSWORD_DEFAULT);
             $param_id = $_SESSION["id"];
             
-            // Attempt to execute the prepared statement
             if($stmt->execute()){
-                // Password updated successfully. Destroy the session, and redirect to login page
                 session_destroy();
                 header("location: login.php");
                 exit();
@@ -60,14 +43,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 echo "Oops! Something went wrong. Please try again later.";
             }
 
-            // Close statement
             $stmt->close();
         }
     }
     
-    // Close connection
     $mysqli->close();
-} //reset password odone
+}
 ?>
  
 
